50be4d974153263cc08739daccbfd787ec0f6920
sayeedreza99 <sayeedreza99.com>
2020-10-05 02:13:27 +0600

    netlify issue solved

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
     return (
         <div className="header">
             <nav>
-                {/* <a href="/home">Home</a> */}
+                <a href="/home">Home</a>
             </nav>
         </div>
     );
